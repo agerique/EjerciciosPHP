a4573015b476e302d8d2eb678e941378d1787716
Erick <adur.gerique@gmail.com>
2021-09-29 11:40:18 +0200

    Update Ejercicio1.php

--- EjerciciosBloque2/Ejercicio1.php
+++ EjerciciosBloque2/Ejercicio1.php
@@ -1,5 +1,60 @@
-<?php
-$naturales = [];
-for ($i=0; $i < 10; $i++) { 
-    $naturales[$i]=$i;
-}
+<html>
+
+<head>
+
+</head>
+
+<body>
+    <?php
+    $naturales = [];
+    $segundo = [];
+    for ($i = 0; $i < 10; $i++) {
+        $naturales[$i] = $i;
+    }
+
+    function calcularFactorial($num)
+    {
+        $factorial = 1;
+        for ($i = 1; $i <= $num; $i++) {
+            $factorial = $factorial * $i;
+        }
+        return $factorial;
+    }
+    for ($i = 0; $i < sizeof($naturales); $i++) {
+        $segundo[$i] = calcularFactorial($naturales[$i]);
+    }
+    function hacerTabla($array)
+    {
+        $texto = "";
+        for ($i = 0; $i < sizeof($array); $i++) {
+            $texto = $texto . "<td>" . $array[$i] . "</td>";
+        }
+        return $texto;
+    }
+    print("
+<table class='default'>
+<tr>
+<th>Numeros Naturales</th>" . hacerTabla($naturales) . "
+</tr>
+<tr>
+<th>Primeros Factoriales</th>" . hacerTabla($segundo) . "
+</tr>
+</table>
+");
+    ?>
+</body>
+<style>
+    table {
+        border: 1px solid #000;
+    }
+
+    th,
+    td {
+        text-align: center;
+        vertical-align: top;
+        border: 1px solid #000;
+        border-spacing: 0;
+    }
+</style>
+
+</html>
